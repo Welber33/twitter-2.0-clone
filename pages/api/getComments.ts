a77012996339e22431f911b75688a258c90c19ce
Welber33 <jwwork21@gmail.com>
2022-09-08 12:01:21 -0300

    Solving problems of loading tweet comments

--- pages/api/getComments.ts
+++ pages/api/getComments.ts
@@ -5,9 +5,9 @@ import { sanityClient } from '../../sanity'
 import { Comment } from '../../typings'
 
 const commentQuery = groq`
-  *[_type == "comment" && references(*[_type == 'tweet' && _id == $tweetId]._id)] {
-    _id,
-    ...
+  *[_type == "comment" && references(*[_type == "tweet" && _id == $tweetId]._id)]{
+      _id,
+      ...
   } | order(_createdAt desc)
 `
 
@@ -17,12 +17,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
   const { tweetId } = req.query
 
   const comments: Comment[] = await sanityClient.fetch(commentQuery, {
     tweetId,
   })
-
   res.status(200).json(comments)
 }
--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,9 +1,9 @@
 import { Comment } from '../typings'
 
 export const fetchComments = async (tweetId: string) => {
-  const res = await fetch(`/api/getcomments?tweetId=${tweetId}`)
+  const res = await fetch(`api/getComments?tweetId=${tweetId}`)
 
   const comments: Comment[] = await res.json()
 
-  return comments 
+  return comments
 }
